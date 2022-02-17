142fe301928f16c63daaf675b9c70255ba3834da
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-02-17 09:51:33 -0300

    HUD and Hit Points
    Hit Points Enemy
    Hit Points VFX
    HitEnemy Method
    Text Score
    Text Mesh Pro Dependency

--- EJEMPLO - 3D FLIGHT/Assets/Scripts/Enemy.cs
+++ EJEMPLO - 3D FLIGHT/Assets/Scripts/Enemy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathVfx;
+    [SerializeField] private GameObject hitVfx;
     [SerializeField] private Transform parent;
-    [SerializeField] private int hitScore  = 100;
+    [SerializeField] private int hitScore = 100;
+    [SerializeField] private int hitPoints = 300;
     ScoreBoard scoreBoard;
 
     private void Start()
@@ -16,7 +18,15 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         scoreBoard.IncreaseScore(hitScore);
-        DestroyEnemy();
+        hitPoints -= hitScore;
+        if (hitPoints < 0)
+        {
+            DestroyEnemy();
+        }else
+        {
+            GameObject newVfx = Instantiate(hitVfx, transform.position, Quaternion.identity);
+            newVfx.transform.parent = parent;
+        }
     }
 
     private void DestroyEnemy()
--- EJEMPLO - 3D FLIGHT/Assets/Scripts/ScoreBoard.cs
+++ EJEMPLO - 3D FLIGHT/Assets/Scripts/ScoreBoard.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+    private void Start() {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "START";
+    }
+
     public void IncreaseScore(int valueToIncrease)
     {
         score += valueToIncrease;
-        Debug.Log(score);
+        scoreText.text = score.ToString();
     }
 }
